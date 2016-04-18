03bc88929aff646d504888cdf99e59ec188697cf
Ronaldo Pontes <EMAIL>
2016-04-18 10:41:41 +0100

    export settings

--- source/env/gamesys/exports.sh
+++ source/env/gamesys/exports.sh
@@ -2,7 +2,7 @@
 
 export SPLUNK_HOME=/Applications/Splunk
 export PATH=$SPLUNK_HOME/bin:$PATH
-export DOCKER_HOST=tcp://192.168.99.103:2376
+export DOCKER_HOST=tcp://192.168.99.102:2376
 export DOCKER_CERT_PATH=/Users/ronaldo.pontes/.docker/machine/machines/dinghy
 export DOCKER_TLS_VERIFY=1
 export DOCKER_MACHINE_NAME=dinghy
